9f5569fb2d4a62db100242c138f44d88d10ee4c1
CodingWithAishik <banerjeeaishik05@gmail.com>
2025-12-04 00:24:39 +0530

    YESSSSSSS

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const meetingRoutes = require('./routes/meeting.routes');
 connectTodb();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
